e98178bceb9d3d31e57365d79f55c23a0388fea8
Divine Touch <divine.touch07@gmail.com>
2020-01-05 16:14:21 -0600

    Initial Set Up

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import './screens/products_overview_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyShop',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        accentColor: Colors.deepOrange,
+        fontFamily: 'Lato',
+      ),
+      home: ProductsOverviewScreen(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MyShop'),
+      ),
+      body: Center(
+        child: Text('Let\'s build a shop!'),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import '../widgets/product_detail_screen.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final imageUrl;
+  ProductItem({this.id, this.title, this.imageUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10.0),
+      child: GridTile(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (_) => ProductDetailScreen(
+                  title: title,
+                ),
+              ),
+            );
+          },
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            icon: Icon(
+              Icons.favorite,
+            ),
+            onPressed: () {},
+            color: Theme.of(context).accentColor,
+          ),
+          title: FittedBox(
+            child: Text(
+              title,
+              textAlign: TextAlign.center,
+            ),
+          ),
+          trailing: IconButton(
+            icon: Icon(
+              Icons.shopping_cart,
+            ),
+            onPressed: () {},
+            color: Theme.of(context).accentColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
