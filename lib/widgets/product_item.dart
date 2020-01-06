c251ccf7aa7578d9e39fd90c7428c33c1e3efa76
Divine Touch <divine.touch07@gmail.com>
2020-01-05 19:05:24 -0600

    only rebuild favorite icon instead of the whole product item widget

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -7,24 +7,24 @@ import '../providers/product.dart';
 class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // final product = Provider.of<Product>(context);
-    return Consumer<Product>(
-      builder: (ctx, product, child) => ClipRRect(
-        borderRadius: BorderRadius.circular(10.0),
-        child: GridTile(
-          child: GestureDetector(
-            onTap: () {
-              Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
-                  arguments: product.id);
-            },
-            child: Image.network(
-              product.imageUrl,
-              fit: BoxFit.cover,
-            ),
+    final product = Provider.of<Product>(context);
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10.0),
+      child: GridTile(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
+                arguments: product.id);
+          },
+          child: Image.network(
+            product.imageUrl,
+            fit: BoxFit.cover,
           ),
-          footer: GridTileBar(
-            backgroundColor: Colors.black87,
-            leading: IconButton(
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: Consumer<Product>(
+            builder: (ctx, product, child) => IconButton(
               icon: Icon(
                   product.isFavorite ? Icons.favorite : Icons.favorite_border),
               onPressed: () {
@@ -32,19 +32,19 @@ class ProductItem extends StatelessWidget {
               },
               color: Theme.of(context).accentColor,
             ),
-            title: FittedBox(
-              child: Text(
-                product.title,
-                textAlign: TextAlign.center,
-              ),
+          ),
+          title: FittedBox(
+            child: Text(
+              product.title,
+              textAlign: TextAlign.center,
             ),
-            trailing: IconButton(
-              icon: Icon(
-                Icons.shopping_cart,
-              ),
-              onPressed: () {},
-              color: Theme.of(context).accentColor,
+          ),
+          trailing: IconButton(
+            icon: Icon(
+              Icons.shopping_cart,
             ),
+            onPressed: () {},
+            color: Theme.of(context).accentColor,
           ),
         ),
       ),
